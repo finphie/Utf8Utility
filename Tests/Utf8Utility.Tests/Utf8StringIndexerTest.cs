0f3ac88cd2d76b2cc54e8432b8d779ae15e4d250
finphie <19237170+finphie@users.noreply.github.com>
2021-04-03 16:38:10 +0900

    Utf8Stringのインデクサーに関するテストを追加

---
+++ Tests/Utf8Utility.Tests/Utf8StringIndexerTest.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace Utf8Utility.Tests
+{
+    public sealed class Utf8StringIndexerTest
+    {
+        [Fact]
+        public void IntIndex()
+        {
+            var x = new Utf8String("abc");
+            x[0].Should().Be((byte)'a');
+            x[1].Should().Be((byte)'b');
+            x[2].Should().Be((byte)'c');
+        }
+
+#if NET5_0_OR_GREATER
+        [Fact]
+        public void Index()
+        {
+            var bytes = Encoding.UTF8.GetBytes("abc");
+            var x = new Utf8String(bytes);
+            x[^1].ToArray().Should().BeEquivalentTo(bytes[^1]);
+        }
+
+        [Fact]
+        public void Range()
+        {
+            var bytes = Encoding.UTF8.GetBytes("abc");
+            var x = new Utf8String(bytes);
+            x[1..].ToArray().Should().BeEquivalentTo(bytes[1..]);
+        }
+#endif
+    }
+}
