5b94b2afbdbe1996819f65a00e786b3938e8d077
finphie <19237170+finphie@users.noreply.github.com>
2021-04-10 10:38:36 +0900

    コメント追加

--- Source/Utf8Utility/Helpers/ThrowHelper.cs
+++ Source/Utf8Utility/Helpers/ThrowHelper.cs
@@ -1,13 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 
 namespace Utf8Utility.Helpers
 {
+    /// <summary>
+    /// 例外をスローするためのヘルパークラスです。
+    /// </summary>
     static class ThrowHelper
     {
+        /// <summary>
+        /// 新しい<see cref="ArgumentOutOfRangeException"/>例外をスローします。
+        /// </summary>
+        /// <param name="paramName">引数名</param>
+        /// <exception cref="ArgumentOutOfRangeException">常にこの例外をスローします。</exception>
+        [DebuggerHidden]
+        [DoesNotReturn]
         public static void ThrowArgumentOutOfRangeException(string paramName)
             => throw new ArgumentOutOfRangeException(paramName);
     }
