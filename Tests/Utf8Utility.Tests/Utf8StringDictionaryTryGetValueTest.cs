87519e36eb2485c745d88f15b20e0b668389a598
finphie <19237170+finphie@users.noreply.github.com>
2021-04-10 12:46:52 +0900

    Utf8StringDictionaryのTryGetValueメソッドに関するテストを追加

---
+++ Tests/Utf8Utility.Tests/Utf8StringDictionaryTryGetValueTest.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Utf8Utility.Tests
+{
+    public class Utf8StringDictionaryTryGetValueTest
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData("abc")]
+        public void 存在するキーを追加_trueを返す(string key)
+        {
+            var utf8Dict = new Utf8StringDictionary<int>();
+            var utf8Key = new Utf8String(key);
+            utf8Dict.Add(utf8Key, 1);
+
+            utf8Dict.TryGetValue(utf8Key, out var utf8DictValue).Should().BeTrue();
+            utf8DictValue.Should().Be(1);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("abc")]
+        public void 存在するキーを2回追加_trueを返す(string key)
+        {
+            var utf8Dict = new Utf8StringDictionary<int>();
+            var utf8Key = new Utf8String(key);
+
+            for (var i = 0; i < 2; i++)
+            {
+                utf8Dict.Add(utf8Key, 1);
+
+                utf8Dict.TryGetValue(utf8Key, out var utf8DictValue).Should().BeTrue();
+                utf8DictValue.Should().Be(1);
+            }
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("abc")]
+        public void 存在しないキー_falseを返す(string key)
+        {
+            var utf8Dict = new Utf8StringDictionary<int>();
+            var utf8Key = new Utf8String(key);
+            utf8Dict.TryGetValue(utf8Key, out var utf8DictValue).Should().BeFalse();
+            utf8DictValue.Should().Be(default);
+        }
+    }
+}
