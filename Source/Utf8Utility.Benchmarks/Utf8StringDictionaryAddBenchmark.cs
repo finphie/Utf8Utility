e566cfd6a3164ef5d43ffd365a65827f88efddb4
finphie <19237170+finphie@users.noreply.github.com>
2021-05-22 11:44:34 +0900

    RuntimeMonikerの指定を修正 (#28)

--- Source/Utf8Utility.Benchmarks/Utf8StringDictionaryAddBenchmark.cs
+++ Source/Utf8Utility.Benchmarks/Utf8StringDictionaryAddBenchmark.cs
@@ -6,7 +6,7 @@ using Utf8Utility.Benchmarks.Helpers;
 
 namespace Utf8Utility.Benchmarks
 {
-    [SimpleJob(RuntimeMoniker.NetCoreApp50)]
+    [SimpleJob(RuntimeMoniker.Net50)]
     [MemoryDiagnoser]
     public class Utf8StringDictionaryAddBenchmark
     {
