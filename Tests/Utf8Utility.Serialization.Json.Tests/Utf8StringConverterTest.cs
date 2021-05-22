6f15d04c2c8002efb327250bdd8b97c4b2251aa4
finphie <19237170+finphie@users.noreply.github.com>
2021-05-22 17:17:06 +0900

    JSONシリアライズに関するテストを追加 (#30)

---
+++ Tests/Utf8Utility.Serialization.Json.Tests/Utf8StringConverterTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using FluentAssertions;
+using Xunit;
+
+namespace Utf8Utility.Serialization.Json.Tests
+{
+    public sealed class Utf8StringConverterTest
+    {
+        static readonly JsonSerializerOptions Options = new();
+
+        static Utf8StringConverterTest() => Options.Converters.Add(new Utf8StringConverter());
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("abc")]
+        public void Read(string value)
+        {
+            var json = $"\"{value}\"";
+
+            var deserialized = JsonSerializer.Deserialize<Utf8String>(json, Options);
+            deserialized.Should().Be((Utf8String)value);
+        }
+
+        [Theory]
+        [InlineData("\"\"")]
+        [InlineData("\"abc\"")]
+        public void Write(string json)
+        {
+            var deserialized = JsonSerializer.Deserialize<Utf8String>(json, Options);
+            var serialized = JsonSerializer.Serialize(deserialized, Options);
+
+            serialized.Should().Be(json);
+        }
+    }
+}
