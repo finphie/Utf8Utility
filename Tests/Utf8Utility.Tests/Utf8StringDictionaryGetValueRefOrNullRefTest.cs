2c8d336fa068fb5472ed5188a78be407338624b7
finphie <19237170+finphie@users.noreply.github.com>
2021-06-19 22:04:11 +0900

    GetValueRefOrNullRefのReadOnlySpan<byte>オーバーロードのテストを追加

--- Tests/Utf8Utility.Tests/Utf8StringDictionaryGetValueRefOrNullRefTest.cs
+++ Tests/Utf8Utility.Tests/Utf8StringDictionaryGetValueRefOrNullRefTest.cs
@@ -7,7 +7,7 @@ namespace Utf8Utility.Tests
     public sealed class Utf8StringDictionaryGetValueRefOrNullRefTest
     {
         [Fact]
-        public void 存在するキーの値型アイテムを取得()
+        public void 存在するキー_値型アイテムを取得()
         {
             var utf8Dict = new Utf8StringDictionary<int>();
             var utf8Key = new Utf8String("abc");
@@ -19,10 +19,16 @@ namespace Utf8Utility.Tests
 
             itemRef = 2;
             itemRef.Should().Be(2);
+
+            itemRef = ref utf8Dict.GetValueRefOrNullRef(utf8Key.AsSpan());
+            itemRef.Should().Be(2);
+
+            itemRef = 3;
+            itemRef.Should().Be(3);
         }
 
         [Fact]
-        public void 存在するキーの参照型アイテムを取得()
+        public void 存在するキー_参照型アイテムを取得()
         {
             var utf8Dict = new Utf8StringDictionary<Test>();
             var utf8Key = new Utf8String("abc");
@@ -41,24 +47,39 @@ namespace Utf8Utility.Tests
             itemRef = new(3, "c");
             itemRef.A.Should().Be(3);
             itemRef.B.Should().Be("c");
+
+            itemRef = ref utf8Dict.GetValueRefOrNullRef(utf8Key.AsSpan())!;
+            itemRef.A.Should().Be(3);
+            itemRef.B.Should().Be("c");
+
+            itemRef.A = 4;
+            itemRef.B = "d";
+            itemRef.A.Should().Be(4);
+            itemRef.B.Should().Be("d");
+
+            itemRef = new(5, "e");
+            itemRef.A.Should().Be(5);
+            itemRef.B.Should().Be("e");
         }
 
         [Fact]
-        public void 存在しないキーの値型アイテムを取得()
+        public void 存在しないキー_値型アイテムを取得()
         {
             var utf8Dict = new Utf8StringDictionary<int>();
             var utf8Key = new Utf8String("abc");
 
             Unsafe.IsNullRef(ref utf8Dict.GetValueRefOrNullRef(utf8Key)).Should().BeTrue();
+            Unsafe.IsNullRef(ref utf8Dict.GetValueRefOrNullRef(utf8Key.AsSpan())).Should().BeTrue();
         }
 
         [Fact]
-        public void 存在しないキーの参照型アイテムを取得()
+        public void 存在しないキー_参照型アイテムを取得()
         {
             var utf8Dict = new Utf8StringDictionary<Test>();
             var utf8Key = new Utf8String("abc");
 
             Unsafe.IsNullRef(ref utf8Dict.GetValueRefOrNullRef(utf8Key)).Should().BeTrue();
+            Unsafe.IsNullRef(ref utf8Dict.GetValueRefOrNullRef(utf8Key.AsSpan())).Should().BeTrue();
         }
 
         class Test
