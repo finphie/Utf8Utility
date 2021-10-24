8c524a974dfec7366e7dd8d299e24769af633d23
finphie <19237170+finphie@users.noreply.github.com>
2021-10-24 12:55:31 +0900

    トップレベルステートメント (#68)

--- Source/Utf8Utility.Benchmarks/Program.cs
+++ Source/Utf8Utility.Benchmarks/Program.cs
@@ -1,12 +1,3 @@
 ﻿using BenchmarkDotNet.Running;
 
-namespace Utf8Utility.Benchmarks;
-
-/// <summary>
-/// ベンチマーク
-/// </summary>
-class Program
-{
-    static void Main(string[] args)
-        => BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
-}
+BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
