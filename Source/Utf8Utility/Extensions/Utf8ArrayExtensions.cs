7b5995b9b0e9d832e333e2fc4593e94402e54075
finphie <19237170+finphie@users.noreply.github.com>
2021-10-29 20:48:54 +0900

    最初の1文字のバイト数を取得するメソッドを追加

---
+++ Source/Utf8Utility/Extensions/Utf8ArrayExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System.Runtime.CompilerServices;
+using Utf8Utility.Helpers;
+
+namespace Utf8Utility.Extensions;
+
+/// <summary>
+/// <see cref="Utf8Array"/>構造体の拡張メソッドです。
+/// </summary>
+public static class Utf8ArrayExtensions
+{
+    /// <summary>
+    /// 最初の1文字のバイト数を取得します。
+    /// </summary>
+    /// <param name="array">UTF-8配列</param>
+    /// <returns>最初の1文字のバイト数</returns>
+    /// <exception cref="InvalidOperationException">バイト数の取得に失敗した場合、この例外をスローします。</exception>
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static int GetFirstCharByteCount(this Utf8Array array)
+    {
+        var first = array.DangerousGetByte();
+
+        if (first < 0x80)
+        {
+            return 1;
+        }
+
+        if ((first & 0xE0) == 0xC0)
+        {
+            return 2;
+        }
+
+        if ((first & 0xF0) == 0xE0)
+        {
+            return 3;
+        }
+
+        if ((first & 0xF8) == 0xF0)
+        {
+            return 4;
+        }
+
+        ThrowHelper.ThrowInvalidUtf8SequenceException(nameof(array));
+        return default;
+    }
+}
