c1404bb5732a9b422cb9aa59522036624f703069
finphie <19237170+finphie@users.noreply.github.com>
2021-10-31 13:15:02 +0900

    Update UnicodeUtility.cs

--- Source/Utf8Utility/Helpers/UnicodeUtility.cs
+++ Source/Utf8Utility/Helpers/UnicodeUtility.cs
@@ -1,5 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
+// https://github.com/dotnet/runtime/blob/f9a24499a1168d391cbf61ab57470a5a974845bb/src/libraries/System.Private.CoreLib/src/System/Text/UnicodeUtility.cs
 
 using System.Runtime.CompilerServices;
 
