d52d8102bd42019b51ae5b6425561c24690e823a
finphie <19237170+finphie@users.noreply.github.com>
2021-11-03 12:52:04 +0900

    Update Utf8ArrayCompareTest.cs

--- Tests/Utf8Utility.Tests/Utf8ArrayCompareTest.cs
+++ Tests/Utf8Utility.Tests/Utf8ArrayCompareTest.cs
@@ -19,12 +19,13 @@ public sealed class Utf8ArrayCompareTest
     [InlineData("aα")]
     [InlineData("aあ")]
     [InlineData("a𩸽")]
-    public void 同じ文字列_CurrentCulture_0を返す(string value)
+    public void 同じ文字列_0を返す(string value)
     {
         var x1 = new Utf8Array(value);
         var x2 = new Utf8Array(value);
 
         Utf8Array.Compare(x1, x2).Should().Be(0);
+        Utf8Array.Compare(x1, x2, StringComparison.InvariantCulture).Should().Be(0);
         x1.CompareTo(x2).Should().Be(0);
     }
 
@@ -48,13 +49,42 @@ public sealed class Utf8ArrayCompareTest
     [InlineData("{", "0")]
     [InlineData("0", "a")]
     [InlineData("a", "A")]
-    public void 異なる文字列_CurrentCulture_0より小さい数値を返す(string value1, string value2)
+    public void 異なる文字列_0より小さい数値を返す(string value1, string value2)
     {
         var x1 = new Utf8Array(value1);
         var x2 = new Utf8Array(value2);
 
         Utf8Array.Compare(x1, x2).Should().BeNegative();
+        Utf8Array.Compare(x1, x2, StringComparison.InvariantCulture).Should().BeNegative();
         x1.CompareTo(x2).Should().BeNegative();
+    }
+
+    [Theory]
+    [InlineData("", "a")]
+    [InlineData("", "α")]
+    [InlineData("", "あ")]
+    [InlineData("", "𩸽")]
+    [InlineData("a", "b")]
+    [InlineData("a", "α")]
+    [InlineData("a", "あ")]
+    [InlineData("a", "𩸽")]
+    [InlineData("α", "β")]
+    [InlineData("α", "あ")]
+    [InlineData("α", "𩸽")]
+    [InlineData("あ", "い")]
+    [InlineData("あ", "𩸽")]
+    [InlineData("a", "ab")]
+    [InlineData("ab", "aα")]
+    [InlineData("!", "0")]
+    [InlineData("z", "{")]
+    [InlineData("0", "a")]
+    [InlineData("A", "a")]
+    public void CompareOrdinal(string value1, string value2)
+    {
+        var x1 = new Utf8Array(value1);
+        var x2 = new Utf8Array(value2);
+
+        Utf8Array.CompareOrdinal(x1, x2).Should().BeNegative();
     }
 }
 #endif
