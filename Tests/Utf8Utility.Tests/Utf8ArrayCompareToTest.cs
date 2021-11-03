151b7b16a818d2af09f2bd9c73a4ac26a6697188
finphie <19237170+finphie@users.noreply.github.com>
2021-11-03 09:23:36 +0900

    Update Utf8ArrayCompareToTest.cs

--- Tests/Utf8Utility.Tests/Utf8ArrayCompareToTest.cs
+++ Tests/Utf8Utility.Tests/Utf8ArrayCompareToTest.cs
@@ -26,5 +26,33 @@ public sealed class Utf8ArrayCompareToTest
 
         x1.CompareTo(x2).Should().Be(0);
     }
+
+    [Theory]
+    [InlineData("", "a")]
+    [InlineData("", "α")]
+    [InlineData("", "あ")]
+    [InlineData("", "𩸽")]
+    [InlineData("a", "b")]
+    [InlineData("a", "α")]
+    [InlineData("a", "あ")]
+    [InlineData("a", "𩸽")]
+    [InlineData("α", "β")]
+    [InlineData("α", "あ")]
+    [InlineData("α", "𩸽")]
+    [InlineData("あ", "い")]
+    [InlineData("あ", "𩸽")]
+    [InlineData("a", "ab")]
+    [InlineData("ab", "aα")]
+    [InlineData("!", "0")]
+    [InlineData("{", "0")]
+    [InlineData("0", "A")]
+    [InlineData("a", "A")]
+    public void 異なる文字列_0より小さい数値を返す(string value1, string value2)
+    {
+        var x1 = new Utf8Array(value1);
+        var x2 = new Utf8Array(value2);
+
+        x1.CompareTo(x2).Should().BeNegative();
+    }
 }
 #endif
