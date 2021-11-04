e8ee5c2406bc65f511252e74cb3c8e179cdfb9e9
finphie <19237170+finphie@users.noreply.github.com>
2021-11-05 08:24:28 +0900

    Create Utf8ArrayIsEmptyOrWhiteSpaceTest.cs

---
+++ Tests/Utf8Utility.Tests/Utf8ArrayIsEmptyOrWhiteSpaceTest.cs
@@ -0,0 +1,39 @@
+﻿#if NET6_0_OR_GREATER
+using FluentAssertions;
+using Xunit;
+
+namespace Utf8Utility.Tests;
+
+public sealed class Utf8ArrayIsEmptyOrWhiteSpaceTest
+{
+    // https://github.com/dotnet/runtime/blob/82d667c6572e85945aa3a02d7f98802db587c0d2/src/libraries/Common/tests/Tests/System/StringTests.cs#L26
+    [Theory]
+    [InlineData("")]
+    [InlineData(" ")]
+    [InlineData("　")]
+    [InlineData("\n")]
+    [InlineData("\r")]
+    [InlineData("\t")]
+    [InlineData("\u00A0")]
+    [InlineData("\u000b")]
+    [InlineData("\u000c")]
+    [InlineData("\u0085")]
+    [InlineData("\u1680")]
+    [InlineData(" \n ")]
+    [InlineData("　\n　")]
+    [InlineData("\u1680\u1680")]
+    public void 空白_trueを返す(string value)
+        => new Utf8Array(value).IsEmptyOrWhiteSpace().Should().BeTrue();
+
+    [Theory]
+    [InlineData("a")]
+    [InlineData("α")]
+    [InlineData("あ")]
+    [InlineData("𩸽")]
+    [InlineData(" a ")]
+    [InlineData("\u1680a ")]
+    [InlineData(" 𩸽")]
+    public void 空白以外_falseを返す(string value)
+    => new Utf8Array(value).IsEmptyOrWhiteSpace().Should().BeFalse();
+}
+#endif
