f6d97a9232d7f81e651ae8a55598b5c3b7053862
finphie <19237170+finphie@users.noreply.github.com>
2021-11-07 07:34:15 +0900

    Update Utf8ArrayGetLengthBenchmark.cs

--- Source/Utf8Utility.Benchmarks/Utf8ArrayGetLengthBenchmark.cs
+++ Source/Utf8Utility.Benchmarks/Utf8ArrayGetLengthBenchmark.cs
@@ -71,4 +71,53 @@ public class Utf8ArrayGetLengthBenchmark
 
         return count;
     }
+
+    [Benchmark]
+    public int GetLength_Long2()
+    {
+        const ulong Mask = 0x8080808080808080;
+
+        var count = 0;
+        nuint i = 0;
+        var length = _value.ByteCount - (8 * 4);
+
+        while ((int)i <= length)
+        {
+            ref var value = ref Unsafe.As<byte, ulong>(ref Unsafe.AddByteOffset(ref _value.DangerousGetReference(), i));
+            var value0 = value;
+            var value1 = Unsafe.Add(ref value, 1);
+            var value2 = Unsafe.Add(ref value, 2);
+            var value3 = Unsafe.Add(ref value, 3);
+
+            count += System.Numerics.BitOperations.PopCount(((value0 >> 6) | (~value0 >> 7)) & (Mask >> 7));
+            count += System.Numerics.BitOperations.PopCount(((value1 >> 6) | (~value1 >> 7)) & (Mask >> 7));
+            count += System.Numerics.BitOperations.PopCount(((value2 >> 6) | (~value2 >> 7)) & (Mask >> 7));
+            count += System.Numerics.BitOperations.PopCount(((value3 >> 6) | (~value3 >> 7)) & (Mask >> 7));
+            i += 8 * 4;
+        }
+
+        length = _value.ByteCount - 8;
+
+        while ((int)i <= length)
+        {
+            var value = Unsafe.As<byte, ulong>(ref Unsafe.AddByteOffset(ref _value.DangerousGetReference(), i));
+            var x = ((value >> 6) | (~value >> 7)) & (Mask >> 7);
+            count += System.Numerics.BitOperations.PopCount(x);
+            i += 8;
+        }
+
+        while ((int)i < _value.ByteCount)
+        {
+            var value = Unsafe.AddByteOffset(ref _value.DangerousGetReference(), i);
+
+            if ((value & 0xC0) != 0x80)
+            {
+                count++;
+            }
+
+            i++;
+        }
+
+        return count;
+    }
 }
