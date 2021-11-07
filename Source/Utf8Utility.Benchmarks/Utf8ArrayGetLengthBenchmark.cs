b807409d43c7ece01df23823c4b75bb66b2581d3
finphie <19237170+finphie@users.noreply.github.com>
2021-11-07 10:03:37 +0900

    t

--- Source/Utf8Utility.Benchmarks/Utf8ArrayGetLengthBenchmark.cs
+++ Source/Utf8Utility.Benchmarks/Utf8ArrayGetLengthBenchmark.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Numerics;
+using System.Runtime.CompilerServices;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Jobs;
 
@@ -20,18 +21,18 @@ public class Utf8ArrayGetLengthBenchmark
     public int GetLength_Loop()
     {
         var count = 0;
-        nuint i = 0;
+        nuint index = 0;
 
-        while ((int)i < _value.ByteCount)
+        while ((int)index < _value.ByteCount)
         {
-            var value = Unsafe.AddByteOffset(ref _value.DangerousGetReference(), i);
+            var value = Unsafe.AddByteOffset(ref _value.DangerousGetReference(), index);
 
             if ((value & 0xC0) != 0x80)
             {
                 count++;
             }
 
-            i++;
+            index++;
         }
 
         return count;
@@ -46,76 +47,27 @@ public class Utf8ArrayGetLengthBenchmark
         const ulong Mask = 0x8080808080808080;
 
         var count = 0;
-        nuint i = 0;
-        var length = _value.ByteCount - 8;
+        nuint index = 0;
+        var length = _value.ByteCount - sizeof(ulong);
 
-        while ((int)i <= length)
+        while ((int)index <= length)
         {
-            var value = Unsafe.As<byte, ulong>(ref Unsafe.AddByteOffset(ref _value.DangerousGetReference(), i));
+            var value = Unsafe.As<byte, ulong>(ref Unsafe.AddByteOffset(ref _value.DangerousGetReference(), index));
             var x = ((value >> 6) | (~value >> 7)) & (Mask >> 7);
-            count += System.Numerics.BitOperations.PopCount(x);
-            i += 8;
+            count += BitOperations.PopCount(x);
+            index += sizeof(ulong);
         }
 
-        while ((int)i < _value.ByteCount)
+        while ((int)index < _value.ByteCount)
         {
-            var value = Unsafe.AddByteOffset(ref _value.DangerousGetReference(), i);
+            var value = Unsafe.AddByteOffset(ref _value.DangerousGetReference(), index);
 
             if ((value & 0xC0) != 0x80)
             {
                 count++;
             }
 
-            i++;
-        }
-
-        return count;
-    }
-
-    [Benchmark]
-    public int GetLength_Long2()
-    {
-        const ulong Mask = 0x8080808080808080;
-
-        var count = 0;
-        nuint i = 0;
-        var length = _value.ByteCount - (8 * 4);
-
-        while ((int)i <= length)
-        {
-            ref var value = ref Unsafe.As<byte, ulong>(ref Unsafe.AddByteOffset(ref _value.DangerousGetReference(), i));
-            var value0 = value;
-            var value1 = Unsafe.Add(ref value, 1);
-            var value2 = Unsafe.Add(ref value, 2);
-            var value3 = Unsafe.Add(ref value, 3);
-
-            count += System.Numerics.BitOperations.PopCount(((value0 >> 6) | (~value0 >> 7)) & (Mask >> 7));
-            count += System.Numerics.BitOperations.PopCount(((value1 >> 6) | (~value1 >> 7)) & (Mask >> 7));
-            count += System.Numerics.BitOperations.PopCount(((value2 >> 6) | (~value2 >> 7)) & (Mask >> 7));
-            count += System.Numerics.BitOperations.PopCount(((value3 >> 6) | (~value3 >> 7)) & (Mask >> 7));
-            i += 8 * 4;
-        }
-
-        length = _value.ByteCount - 8;
-
-        while ((int)i <= length)
-        {
-            var value = Unsafe.As<byte, ulong>(ref Unsafe.AddByteOffset(ref _value.DangerousGetReference(), i));
-            var x = ((value >> 6) | (~value >> 7)) & (Mask >> 7);
-            count += System.Numerics.BitOperations.PopCount(x);
-            i += 8;
-        }
-
-        while ((int)i < _value.ByteCount)
-        {
-            var value = Unsafe.AddByteOffset(ref _value.DangerousGetReference(), i);
-
-            if ((value & 0xC0) != 0x80)
-            {
-                count++;
-            }
-
-            i++;
+            index++;
         }
 
         return count;
