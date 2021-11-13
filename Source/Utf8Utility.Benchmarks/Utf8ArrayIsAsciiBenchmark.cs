f01aa89e26141f9140989edd2feade321b621c52
finphie <19237170+finphie@users.noreply.github.com>
2021-11-14 06:29:51 +0900

    Create Utf8ArrayIsAsciiBenchmark.cs

---
+++ Source/Utf8Utility.Benchmarks/Utf8ArrayIsAsciiBenchmark.cs
@@ -0,0 +1,31 @@
+﻿using BenchmarkDotNet.Attributes;
+using Utf8Utility.Benchmarks.Helpers;
+
+namespace Utf8Utility.Benchmarks;
+
+[Config(typeof(BenchmarkConfig))]
+public class Utf8ArrayIsAsciiBenchmark
+{
+    Utf8Array _value;
+
+    [Params(4, 16, 100)]
+    public int Length { get; set; }
+
+    [GlobalSetup]
+    public void Setup() => _value = new(StringHelper.GetAsciiRandomString(Length));
+
+    [Benchmark]
+    public void IsAscii() => _value.IsAscii();
+
+    [Benchmark]
+    public void IsAscii2() => _value.IsAscii2();
+
+    [Benchmark]
+    public void IsAscii5() => _value.IsAscii5();
+
+    [Benchmark]
+    public void IsAsciiAvx() => _value.IsAsciiAvx();
+
+    [Benchmark]
+    public void IsAsciiSse2() => _value.IsAsciiSse2();
+}
