9fcdd0976f93e729a810974691cf81ea17e2a6a1
finphie <19237170+finphie@users.noreply.github.com>
2021-11-14 17:32:43 +0900

    Update Utf8ArrayIsAsciiBenchmark.cs

--- Source/Utf8Utility.Benchmarks/Utf8ArrayIsAsciiBenchmark.cs
+++ Source/Utf8Utility.Benchmarks/Utf8ArrayIsAsciiBenchmark.cs
@@ -1,7 +1,9 @@
 ﻿using System.Runtime.CompilerServices;
 using System.Runtime.Intrinsics;
 using System.Runtime.Intrinsics.X86;
+using System.Text;
 using BenchmarkDotNet.Attributes;
+using Microsoft.Toolkit.HighPerformance;
 using Utf8Utility.Benchmarks.Helpers;
 
 namespace Utf8Utility.Benchmarks;
@@ -9,18 +11,21 @@ namespace Utf8Utility.Benchmarks;
 [Config(typeof(BenchmarkConfig))]
 public class Utf8ArrayIsAsciiBenchmark
 {
-    Utf8Array _value;
+#nullable disable
+    byte[] _value;
+#nullable restore
 
     [Params(4, 16, 100)]
     public int Length { get; set; }
 
     [GlobalSetup]
-    public void Setup() => _value = new(StringHelper.GetAsciiRandomString(Length));
+    public void Setup()
+        => _value = Encoding.ASCII.GetBytes(StringHelper.GetAsciiRandomString(Length));
 
     [Benchmark]
     public bool IsAscii_Byte()
     {
-        for (nuint index = 0; (int)index < _value.ByteCount; index++)
+        for (nuint index = 0; (int)index < _value.Length; index++)
         {
             if ((sbyte)Unsafe.AddByteOffset(ref _value.DangerousGetReference(), index) < 0)
             {
@@ -35,125 +40,211 @@ public class Utf8ArrayIsAsciiBenchmark
     public bool IsAscii_ULong()
     {
         nuint index = 0;
-        var mask1 = 0UL;
+        var mask8 = 0UL;
 
-        while ((int)index < _value.ByteCount - sizeof(ulong))
+        ref var first = ref _value.DangerousGetReference();
+
+        if (_value.Length - (int)index >= sizeof(ulong))
         {
-            var value = Unsafe.As<byte, ulong>(ref Unsafe.AddByteOffset(ref _value.DangerousGetReference(), index));
+            ref var current = ref Unsafe.As<byte, ulong>(ref first);
+            var endIndex = _value.Length - sizeof(ulong);
 
-            mask1 |= value;
-            index += sizeof(ulong);
+            do
+            {
+                mask8 |= Unsafe.AddByteOffset(ref current, index);
+                index += sizeof(ulong);
+            }
+            while ((int)index < endIndex);
         }
 
-        byte mask2 = 0;
-
-        while ((int)index < _value.ByteCount)
+        if (_value.Length - (int)index >= sizeof(uint))
         {
-            mask2 |= Unsafe.AddByteOffset(ref _value.DangerousGetReference(), index);
-            index++;
+            mask8 |= Unsafe.As<byte, uint>(ref Unsafe.AddByteOffset(ref first, index));
+            index += sizeof(uint);
         }
 
-        return ((mask1 | mask2) & 0x8080808080808080) == 0;
+        if (_value.Length - (int)index >= sizeof(ushort))
+        {
+            mask8 |= Unsafe.As<byte, ushort>(ref Unsafe.AddByteOffset(ref first, index));
+            index += sizeof(ushort);
+        }
+
+        if ((int)index < _value.Length)
+        {
+            mask8 |= Unsafe.AddByteOffset(ref first, index);
+        }
+
+        return (mask8 & 0x8080808080808080) == 0;
     }
 
     [Benchmark]
     public bool IsAscii_Ulong4()
     {
         nuint index = 0;
-        var mask1 = 0UL;
-        var mask2 = 0UL;
-        var mask3 = 0UL;
-        var mask4 = 0UL;
+        var maskA = 0UL;
+        var maskB = 0UL;
+        var maskC = 0UL;
+        var maskD = 0UL;
 
-        while ((int)index < _value.ByteCount - (sizeof(ulong) * 4))
+        ref var first = ref _value.DangerousGetReference();
+
+        if (_value.Length >= sizeof(ulong) * 4)
         {
-            ref var valueStart = ref Unsafe.As<byte, ulong>(ref Unsafe.AddByteOffset(ref _value.DangerousGetReference(), index));
-            mask1 |= valueStart;
-            mask2 |= Unsafe.Add(ref valueStart, 1);
-            mask3 |= Unsafe.Add(ref valueStart, 2);
-            mask4 |= Unsafe.Add(ref valueStart, 3);
+            ref var current = ref Unsafe.As<byte, ulong>(ref first);
+            var endIndex = _value.Length - (sizeof(ulong) * 4);
 
-            index += sizeof(ulong) * 4;
+            do
+            {
+                maskA |= Unsafe.AddByteOffset(ref current, index);
+                maskB |= Unsafe.AddByteOffset(ref current, index + sizeof(ulong));
+                maskC |= Unsafe.AddByteOffset(ref current, index + (sizeof(ulong) * 2));
+                maskD |= Unsafe.AddByteOffset(ref current, index + (sizeof(ulong) * 3));
+                index += sizeof(ulong) * 4;
+            }
+            while ((int)index < endIndex);
         }
 
-        while ((int)index < _value.ByteCount - sizeof(ulong))
+        if (_value.Length - (int)index >= sizeof(ulong) * 2)
         {
-            var value = Unsafe.As<byte, ulong>(ref Unsafe.AddByteOffset(ref _value.DangerousGetReference(), index));
+            ref var current = ref Unsafe.As<byte, ulong>(ref Unsafe.AddByteOffset(ref first, index));
 
-            mask1 |= value;
+            maskA |= current;
+            maskA |= Unsafe.Add(ref current, 1);
+            index += sizeof(ulong) * 2;
+        }
+
+        if (_value.Length - (int)index >= sizeof(ulong))
+        {
+            maskA |= Unsafe.As<byte, ulong>(ref Unsafe.AddByteOffset(ref first, index));
             index += sizeof(ulong);
         }
 
-        byte mask5 = 0;
-
-        while ((int)index < _value.ByteCount)
+        if (_value.Length - (int)index >= sizeof(uint))
         {
-            mask5 |= Unsafe.AddByteOffset(ref _value.DangerousGetReference(), index);
-            index++;
+            maskA |= Unsafe.As<byte, uint>(ref Unsafe.AddByteOffset(ref first, index));
+            index += sizeof(uint);
         }
 
-        return ((mask1 | mask2 | mask3 | mask4 | mask5) & 0x8080808080808080) == 0;
+        if (_value.Length - (int)index >= sizeof(ushort))
+        {
+            maskA |= Unsafe.As<byte, ushort>(ref Unsafe.AddByteOffset(ref first, index));
+            index += sizeof(ushort);
+        }
+
+        if ((int)index < _value.Length)
+        {
+            maskA |= Unsafe.AddByteOffset(ref first, index);
+        }
+
+        return ((maskA | maskB | maskC | maskD) & 0x8080808080808080) == 0;
     }
 
     [Benchmark]
     public unsafe bool IsAscii_Sse2()
     {
-        var index = 0;
-        var mask1 = Vector128<byte>.Zero;
+        nuint index = 0;
+        var mask16 = Vector128<byte>.Zero;
 
-        if (_value.ByteCount >= 16)
+        ref var first = ref _value.DangerousGetReference();
+
+        if (_value.Length >= Vector128<byte>.Count)
         {
-            fixed (byte* ptr = &_value.DangerousGetReference())
+            var endIndex = _value.Length - Vector128<byte>.Count;
+            ref var current = ref Unsafe.As<byte, Vector128<byte>>(ref first);
+
+            do
             {
-                for (; index <= _value.ByteCount - 16; index += 16)
-                {
-                    mask1 = Sse2.Or(mask1, Sse2.LoadVector128(ptr + index));
-                }
+                mask16 = Sse2.Or(mask16, Unsafe.AddByteOffset(ref current, index));
+                index += (uint)Vector128<byte>.Count;
             }
+            while ((int)index <= endIndex);
         }
 
-        var result = Sse2.MoveMask(mask1);
-        sbyte mask2 = 0;
+        var result = Sse2.MoveMask(mask16);
+        var mask8 = 0UL;
 
-        for (; index < _value.ByteCount; index++)
+        if (_value.Length - (int)index >= sizeof(ulong))
         {
-            mask2 |= (sbyte)Unsafe.AddByteOffset(ref _value.DangerousGetReference(), (nint)(uint)index);
+            mask8 |= Unsafe.As<byte, ulong>(ref Unsafe.AddByteOffset(ref first, index));
+            index += sizeof(ulong);
         }
 
-        result |= mask2 & 0x80;
+        if (_value.Length - (int)index >= sizeof(uint))
+        {
+            mask8 |= Unsafe.As<byte, uint>(ref Unsafe.AddByteOffset(ref first, index));
+            index += sizeof(uint);
+        }
 
-        return result == 0;
+        if (_value.Length - (int)index >= sizeof(ushort))
+        {
+            mask8 |= Unsafe.As<byte, ushort>(ref Unsafe.AddByteOffset(ref first, index));
+            index += sizeof(ushort);
+        }
+
+        if ((int)index < _value.Length)
+        {
+            mask8 |= Unsafe.AddByteOffset(ref first, index);
+        }
+
+        return ((uint)result | (mask8 & 0x8080808080808080)) == 0;
     }
 
     [Benchmark]
     public unsafe bool IsAscii_Avx2()
     {
         nuint index = 0;
-        var mask1 = Vector256<byte>.Zero;
+        var mask32 = Vector256<byte>.Zero;
 
-        if (_value.ByteCount >= Vector256<byte>.Count)
+        ref var first = ref _value.DangerousGetReference();
+
+        if (_value.Length >= Vector256<byte>.Count)
         {
-            var endIndex = _value.ByteCount - Vector256<byte>.Count;
-            ref var first = ref Unsafe.As<byte, Vector256<byte>>(ref _value.DangerousGetReference());
+            var endIndex = _value.Length - Vector256<byte>.Count;
+            ref var current = ref Unsafe.As<byte, Vector256<byte>>(ref first);
 
             do
             {
-                mask1 = Avx2.Or(mask1, Unsafe.AddByteOffset(ref first, index));
+                mask32 = Avx2.Or(mask32, Unsafe.AddByteOffset(ref current, index));
                 index += (uint)Vector256<byte>.Count;
             }
             while ((int)index <= endIndex);
         }
 
-        var result = Avx2.MoveMask(mask1);
-        var mask2 = 0;
+        var result = Avx2.MoveMask(mask32);
+        var mask8 = 0UL;
 
-        for (; (int)index < _value.ByteCount; index++)
+        if (_value.Length - (int)index >= sizeof(ulong) * 2)
         {
-            mask2 |= Unsafe.AddByteOffset(ref _value.DangerousGetReference(), index);
+            ref var current = ref Unsafe.As<byte, ulong>(ref Unsafe.AddByteOffset(ref first, index));
+
+            mask8 = current;
+            mask8 |= Unsafe.Add(ref current, 1);
+            index += sizeof(ulong) * 2;
         }
 
-        result |= mask2 & 0x80;
+        if (_value.Length - (int)index >= sizeof(ulong))
+        {
+            mask8 |= Unsafe.As<byte, ulong>(ref Unsafe.AddByteOffset(ref first, index));
+            index += sizeof(ulong);
+        }
 
-        return result == 0;
+        if (_value.Length - (int)index >= sizeof(uint))
+        {
+            mask8 |= Unsafe.As<byte, uint>(ref Unsafe.AddByteOffset(ref first, index));
+            index += sizeof(uint);
+        }
+
+        if (_value.Length - (int)index >= sizeof(ushort))
+        {
+            mask8 |= Unsafe.As<byte, ushort>(ref Unsafe.AddByteOffset(ref first, index));
+            index += sizeof(ushort);
+        }
+
+        if ((int)index < _value.Length)
+        {
+            mask8 |= Unsafe.AddByteOffset(ref first, index);
+        }
+
+        return ((uint)result | (mask8 & 0x8080808080808080)) == 0;
     }
 }
