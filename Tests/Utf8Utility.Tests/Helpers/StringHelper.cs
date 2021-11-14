89963d060a20fc055d45d7b44d691603d5f15d77
finphie <19237170+finphie@users.noreply.github.com>
2021-11-14 18:13:56 +0900

    テスト

---
+++ Tests/Utf8Utility.Tests/Helpers/StringHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace Utf8Utility.Tests.Helpers;
+
+/// <summary>
+/// <see cref="string"/>関連のヘルパークラス。
+/// </summary>
+sealed class StringHelper
+{
+    const int Seed = 20221106;
+
+    static readonly Random Random = new(Seed);
+
+    /// <summary>
+    /// ランダムなAscii文字列を取得します。
+    /// </summary>
+    /// <param name="length">文字数</param>
+    /// <returns>ランダムなAscii文字列</returns>
+    /// <exception cref="ArgumentOutOfRangeException">文字数に0以下が指定された場合、この例外をスローします。</exception>
+    public static string GetAsciiRandomString(int length)
+    {
+        if (length <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(length));
+        }
+
+        var buffer = new byte[length];
+
+        for (var i = 0; i < buffer.Length; i++)
+        {
+            buffer[i] = (byte)Random.Next(0, 0x7F);
+        }
+
+        return Encoding.ASCII.GetString(buffer);
+    }
+}
---
+++ Tests/Utf8Utility.Tests/Utf8ArrayIsAsciiTest.cs
@@ -0,0 +1,34 @@
+﻿#if NET6_0_OR_GREATER
+using FluentAssertions;
+using Utf8Utility.Tests.Helpers;
+using Xunit;
+
+namespace Utf8Utility.Tests;
+
+public sealed class Utf8ArrayIsAsciiTest
+{
+    [Theory]
+    [InlineData(10)]
+    [InlineData(127)]
+    public void Ascii文字_trueを返す(int length)
+    {
+        var value = new Utf8Array(StringHelper.GetAsciiRandomString(length));
+        value.IsAscii().Should().BeTrue();
+    }
+
+    [Theory]
+    [InlineData(10)]
+    [InlineData(127)]
+    public void 非Ascii文字_falseを返す(int length)
+    {
+        var ascii = StringHelper.GetAsciiRandomString(length - 2);
+
+        for (var i = 0; i <= ascii.Length; i++)
+        {
+            var s = ascii.Insert(i, "α");
+            var value = new Utf8Array(s);
+            value.IsAscii().Should().BeFalse($"index: {i}");
+        }
+    }
+}
+#endif
