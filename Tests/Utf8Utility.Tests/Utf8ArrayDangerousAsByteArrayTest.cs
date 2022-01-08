53d534f235c1181f05f11dae75175d7f40a3cf3b
finphie <19237170+finphie@users.noreply.github.com>
2022-01-08 14:04:59 +0900

    DangerousAsByteArray

---
+++ Tests/Utf8Utility.Tests/Utf8ArrayDangerousAsByteArrayTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using FluentAssertions;
+using Xunit;
+
+namespace Utf8Utility.Tests;
+
+public sealed class Utf8ArrayDangerousAsByteArrayTest
+{
+    [Theory]
+    [InlineData("abcdef")]
+    public void 配列を返す(string value)
+    {
+        var utf8Value = Encoding.UTF8.GetBytes(value);
+        var utf8Array = new Utf8Array(utf8Value).DangerousAsByteArray();
+
+        utf8Array.SequenceEqual(utf8Value).Should().BeTrue();
+    }
+}
