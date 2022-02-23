295fcaf7e55cc36588f2ff076ae1f76fdef0a789
finphie <19237170+finphie@users.noreply.github.com>
2022-02-23 20:51:06 +0900

    DOTNET_MULTILEVEL_LOOKUP

--- .github/CheckTargetFramework.ps1
+++ .github/CheckTargetFramework.ps1
@@ -4,19 +4,16 @@ foreach ($file in $files) {
     $xml = [Xml](Get-Content $file -Encoding UTF8)
     $node = $xml.Project.SelectSingleNode('//Project/PropertyGroup/TargetFramework')
 
-    if (-not $node) {
+    if (!$node) {
         $node = $xml.Project.SelectSingleNode('//Project/PropertyGroup/TargetFrameworks')
     }
 
-    $text = $node.InnerText
+    [string] $text = $node.InnerText
     Write-Output "$($file.Name) $text"
 
     $targetFrameworks = $text -Split ';'
 
-    foreach ($targetFramework in $targetFrameworks) {
-        if ($targetFramework -ne 'net6.0') {
-            Write-Output 'DOTNET_MULTILEVEL_LOOKUP=1' | Out-File $Env:GITHUB_ENV -Encoding utf8 -Append
-            return
-        }
+    if (!($targetFrameworks -in 'net6.0')) {
+        Write-Output 'DOTNET_MULTILEVEL_LOOKUP=1' | Out-File $Env:GITHUB_ENV -Encoding utf8 -Append
     }
 }
