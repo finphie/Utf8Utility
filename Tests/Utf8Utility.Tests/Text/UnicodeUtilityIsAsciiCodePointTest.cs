26a07a7b9834fb4a7ec52d09a68157cc7f7f1023
finphie <19237170+finphie@users.noreply.github.com>
2023-09-08 21:14:01 +0900

    Create UnicodeUtilityIsAsciiCodePointTest.cs

---
+++ Tests/Utf8Utility.Tests/Text/UnicodeUtilityIsAsciiCodePointTest.cs
@@ -0,0 +1,24 @@
+﻿using FluentAssertions;
+using Utf8Utility.Text;
+using Xunit;
+
+namespace Utf8Utility.Tests.Text;
+
+public sealed class UnicodeUtilityIsAsciiCodePointTest
+{
+    [Theory]
+    [InlineData(0x00)]
+    [InlineData(0x7F)]
+    public void Ascii文字_trueを返す(byte value)
+        => UnicodeUtility.IsAsciiCodePoint(value).Should().BeTrue();
+
+    [Theory]
+    [InlineData(0xC2)]
+    [InlineData(0xDF)]
+    [InlineData(0xE0)]
+    [InlineData(0xEF)]
+    [InlineData(0xF0)]
+    [InlineData(0xF4)]
+    public void 非Ascii文字_falseを返す(byte value)
+        => UnicodeUtility.IsAsciiCodePoint(value).Should().BeFalse();
+}
